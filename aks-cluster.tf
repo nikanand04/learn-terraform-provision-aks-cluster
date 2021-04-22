8d8c85cd7457c3344fd0632b9441e1646d246e07
Nikita Anand <51712876+nikanand04@users.noreply.github.com>
2021-04-22 14:56:56 -0400

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -2,6 +2,7 @@ resource "random_pet" "prefix" {}
 
 provider "azurerm" {
     features{}
+    
 }
 
 resource "azurerm_resource_group" "default" {
@@ -18,6 +19,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
+  kubernetes_version  = "1.18"  
 
   default_node_pool {
     name            = "default"
